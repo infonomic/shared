d5ab4d43c7d12cc4da47094cf211dfd8c238fc04
Anthony Bouch <anthony@infonomic.io>
2025-08-26 15:54:23 +0700

    feat: separate passwords exports for crypto for node:crypto deps

--- packages/crypto/src/index.ts
+++ packages/crypto/src/index.ts
@@ -1,3 +1,7 @@
+// NOTE: We do NOT include password-related functions here
+// since the argon lib has a dependency on node:crypto,
+// which will cause bundler errors in non-node.js environments
+// (like webpack/turbopack in Next.js)
 export {
   decrypt,
   decryptObject,
@@ -8,8 +12,3 @@ export {
   importKeyFromBase64,
   jwtVerify,
 } from './crypto.js'
-
-export {
-  hashPassword,
-  verifyPassword,
-} from './passwords.js'
